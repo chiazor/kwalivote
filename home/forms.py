110c003f725253b848a29774dc3f1ec39eff7fb8
breezil <kolajubril@gmail.com>
2018-06-22 11:16:00 -0400

    user authentication done

--- home/forms.py
+++ home/forms.py
@@ -1,13 +1,50 @@
 from django import forms
-from .models import home
-from django.forms import ModelForm
-from django.db import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+# from .models import home
+# from django.forms import ModelForm
+# from django.db import models
 
 
-class LoginForm(forms.Form):
-	username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class' : 'form-control'}))
-	password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+# class LoginForm(forms.Form):
+# 	username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class' : 'form-control'}))
+# 	password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
 
-	class Meta:
-		model = home
-		fields = "__all__"
+# 	class Meta:
+# 		model = home
+# 		fields = "__all__"
+
+User = get_user_model()
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2'
+        )
+
+    # save user method
+    def save(self, commit=True):
+        user = super(RegistrationForm,self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']    
+
+        if commit:
+            user.save()
+
+        return user 
--- home/urls.py
+++ home/urls.py
@@ -15,9 +15,42 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
 from . import views
+from django.contrib.auth.views import (
+    login,
+    logout,
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete,
+) 
+
+app_name = "home"
 
 urlpatterns = [
-    url(r'', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', login, {'template_name':'home/login.html'},name="login"),
+    url(r'^logout/$', logout,{'template_name':'index.html'},name='logout'),
+    url(r'^change-password/$',views.change_password, name="change_password"),
+
+    url(r'^reset-password/$', password_reset, {'template_name':'home/reset_password.html',
+                                                    'email_template_name':'home/reset_password_email.html',
+                                                    'post_reset_redirect':'home:password_reset_done',
+                                                    'from_email':'home@django.com',},name='password_reset'),
+
+   
+    url(r'^reset-password/done/$', password_reset_done, {'template_name': 'home/reset_password_done.html'},
+                                                         name='password_reset_done'),
+
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+    password_reset_confirm, {'template_name':'home/reset_password_confirm.html',
+                              'post_reset_redirect': reverse_lazy('home:password_reset_complete')},
+                              name='password_reset_confirm'),
+
+
+    url(r'reset-password/complete/$', password_reset_complete,{'template_name':'home/reset_password_complete.html'}, name='password_reset_complete'),
+                
     # url(r'^poll/', include('poll.urls')),
 ]
